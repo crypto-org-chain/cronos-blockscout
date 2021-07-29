2c1fa03aa0d4b278106d770a8677779ef7a9e918
Mo Husseini <mo.husseini@crypto.com>
2021-07-29 14:06:46 -0400

    Add create and migrate step in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,8 @@ set -e
 
 cd /opt/app
 
+# Create needed for first time usage, won't do anything otherwise
+# migrate is importatnt to run before application
+/usr/local/bin/mix do ecto.create, ecto.migrate
+
 /usr/local/bin/mix phx.server
