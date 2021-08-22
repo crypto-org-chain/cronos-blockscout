8a9b934c55d9b54e80f8c2b51f969546b3804adc
WilliamXieCrypto <william.xie@crypto.com>
2021-08-22 22:33:34 +0800

    Update gettext; code cleaning up

--- apps/block_scout_web/lib/block_scout_web/views/chain_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/chain_view.ex
@@ -3,8 +3,6 @@ defmodule BlockScoutWeb.ChainView do
 
   import Number.Currency, only: [number_to_currency: 1, number_to_currency: 2]
 
-#  alias Explorer.Chain.Supply.TokenBridge
-
   defp market_cap(:standard, %{available_supply: available_supply, usd_value: usd_value})
        when is_nil(available_supply) or is_nil(usd_value) do
     Decimal.new(0)
@@ -21,18 +19,6 @@ defmodule BlockScoutWeb.ChainView do
   defp market_cap(module, exchange_rate) do
     module.market_cap(exchange_rate)
   end
-
-#  defp total_market_cap_from_token_bridge(%{usd_value: usd_value}) do
-#    TokenBridge.token_bridge_market_cap(%{usd_value: usd_value})
-#  end
-#
-#  defp total_market_cap_from_omni_bridge do
-#    TokenBridge.total_market_cap_from_omni_bridge()
-#  end
-#
-#  defp token_bridge_supply? do
-#    if System.get_env("SUPPLY_MODULE") === "TokenBridge", do: true, else: false
-#  end
 
   def format_usd_value(value) do
     "#{format_currency_value(value)} USD"
